71d5bebb849e18c1a87f9f693289ef0a9453f43f
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2024-12-23 22:20:04 -0500

    feat: Initial project structure

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <luajit-2.1/lua.hpp>
+
+int main() {
+  lua_State *L = luaL_newstate();
+  luaL_openlibs(L);
+
+  const char *script = "print('Hello, Lua!')";
+
+  if (luaL_loadstring(L, script) == LUA_OK) {
+    if (lua_pcall(L, 0, 0, 0) == LUA_OK) {
+      lua_pop(L, lua_gettop(L));
+    }
+  }
+
+  lua_close(L);
+  return 0;
+}
