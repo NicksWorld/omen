bfe0848aeac4d34acd0bbd54db8a81515f1f966a
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2024-12-25 22:31:53 -0500

    style: Correct indentation width

--- external/luajit.cmake
+++ external/luajit.cmake
@@ -1,25 +1,25 @@
 set(LUAJIT_INC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/luajit/src)
 if (WIN32)
-    set(LUAJIT_LIB ${CMAKE_CURRENT_SOURCE_DIR}/luajit/src/lua51.lib)
+  set(LUAJIT_LIB ${CMAKE_CURRENT_SOURCE_DIR}/luajit/src/lua51.lib)
 else()
-    set(LUAJIT_LIB ${CMAKE_CURRENT_SOURCE_DIR}/luajit/src/libluajit.a)
+  set(LUAJIT_LIB ${CMAKE_CURRENT_SOURCE_DIR}/luajit/src/libluajit.a)
 endif()
 
 # Build LuaJIT with default settings
 if (WIN32)
-    add_custom_command(
-        OUTPUT ${LUAJIT_LIB}
-        COMMAND msvcbuild.bat
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/luajit/src
-        COMMENT "Building LuaJIT on Windows"
-    )
+  add_custom_command(
+    OUTPUT ${LUAJIT_LIB}
+    COMMAND msvcbuild.bat
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/luajit/src
+    COMMENT "Building LuaJIT on Windows"
+  )
 else()
-    add_custom_command(
-        OUTPUT ${LUAJIT_LIB}
-        COMMAND make
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/luajit/src
-        COMMENT "Building LuaJIT on Unix-like systems"
-    )
+  add_custom_command(
+    OUTPUT ${LUAJIT_LIB}
+    COMMAND make
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/luajit/src
+    COMMENT "Building LuaJIT on Unix-like systems"
+)
 endif()
 add_custom_target(_LuaJIT ALL DEPENDS ${LUAJIT_LIB})
 
@@ -27,7 +27,7 @@ add_custom_target(_LuaJIT ALL DEPENDS ${LUAJIT_LIB})
 # IMPORTED GLOBAL allows it to refer to the static library built from the custom commands
 add_library(LuaJIT STATIC IMPORTED GLOBAL DEPENDS ${LUAJIT_LIB})
 set_target_properties(LuaJIT PROPERTIES
-    IMPORTED_LOCATION "${LUAJIT_LIB}"
-    INTERFACE_INCLUDE_DIRECTORIES "${LUAJIT_INC_DIR}"
+  IMPORTED_LOCATION "${LUAJIT_LIB}"
+  INTERFACE_INCLUDE_DIRECTORIES "${LUAJIT_INC_DIR}"
 )
 add_dependencies(LuaJIT _LuaJIT)
