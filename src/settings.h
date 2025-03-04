c9f2c70748fa4e00c4d4adc022e189a04358afd3
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2025-03-03 22:10:10 -0500

    style: don't indent namespace contents

--- src/settings.h
+++ src/settings.h
@@ -6,21 +6,22 @@
 #include <variant>
 
 namespace omen {
-  /// Represents the result of a fallible settings method.
-  template <typename T>
-  using SettingsResult = std::variant<T, std::string>;
+/// Represents the result of a fallible settings method.
+template <typename T> using SettingsResult = std::variant<T, std::string>;
 
-  /// Core configuration for an Omen Application
-  class EngineSettings {
-  private:
-    EngineSettings() = default;
-  public:
-    /// Name for the application
-    std::string_view app_name = "Omen Application";
-    /// Path of the core script which is used as the Lua entrypoint
-    std::string_view boot_script = "core/boot/boot.lua";
+/// Core configuration for an Omen Application
+class EngineSettings {
+private:
+  EngineSettings() = default;
 
-    /// Loads settings from the current working directory
-    static SettingsResult<std::unique_ptr<EngineSettings>> load();
-  };
+public:
+  ~EngineSettings() = default;
+  /// Name for the application
+  std::string_view app_name = "Omen Application";
+  /// Path of the core script which is used as the Lua entrypoint
+  std::string_view boot_script = "core/boot/boot.lua";
+
+  /// Loads settings from the current working directory
+  static SettingsResult<std::unique_ptr<EngineSettings>> load();
+};
 }
