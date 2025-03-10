0daa5708c69bb6c6f89b7407db05cd81c0366e64
Nicholas McDaniel <nickmcdaniel00@gmail.com>
2025-03-10 11:15:08 -0400

    feat: Inline method to hash a string_view

--- common/include/omen/hash.h
+++ common/include/omen/hash.h
@@ -5,6 +5,10 @@ namespace omen::hash {
 /// Hashes the given data via a non-cryptographic hash
 uint64_t hash_value(const void *data, std::size_t size);
 
+inline uint64_t hash_value(const std::string_view &view) {
+  return hash_value(view.data(), view.size());
+}
+
 /// Represents a hashed value
 ///
 /// The hash used is non-cryptographic and is intended for use with paths and
@@ -13,7 +17,7 @@ class HashId {
 public:
   /// Hash the given string
   explicit HashId(const std::string_view &view)
-      : hash(hash_value(view.data(), view.size())) {};
+      : hash(hash_value(view)) {};
   /// Hash a slice of raw data
   explicit HashId(const void *data, std::size_t size)
       : hash(hash_value(data, size)) {};
